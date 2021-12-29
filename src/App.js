e197a925c88a7ba6d703635726938221714ac5c7
EricPohlers <53621659+EricPohlers@users.noreply.github.com>
2021-12-29 20:14:51 +0100

    removed unnecessary code

--- src/App.js
+++ src/App.js
@@ -15,7 +15,6 @@ export default class App extends Component {
   }
 
   handleChange(event) {
-    console.log(event);
     this.setState({ searchTerm: event.target.value });
   }
 
--- src/TemtemList.js
+++ src/TemtemList.js
@@ -2,20 +2,13 @@ import React, { Component } from 'react';
 import TemtemCard from './TemtemCard';
 
 export default class TemtemList extends Component {
-  // componentDidMount() {
-  //   // this.data.map((temtemData) => {
-  //   //   console.log(temtemData.name);
-  //   // });
-  // }
-
   render() {
     return (
-      <div className="grid gap-4 lg:grid-cols-4  md:grid-cols-2 justify-evenly m-8">
+      <div className="transition-all grid gap-4 lg:grid-cols-4  md:grid-cols-2 justify-evenly m-8">
         <React.StrictMode>
           {this.props.data.map((temtem) => {
             return <TemtemCard key={temtem.number} data={temtem} />;
           })}
-          {/* {return this.createTemtemCard(temtem)})} */}
         </React.StrictMode>
       </div>
     );
