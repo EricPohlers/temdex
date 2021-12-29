ef8d14e5f9c77a43afa1af8f4ebb881483fd52bd
EricPohlers <53621659+EricPohlers@users.noreply.github.com>
2021-12-29 23:10:21 +0100

    mobile fixes

--- src/Types.js
+++ src/Types.js
@@ -4,18 +4,18 @@ export default class Types extends Component {
   render() {
     return (
       <div
-        className={`flex py-4 px-3 font-mono ${
+        className={`flex py-4 px-3 font-mono flex-wrap ${
           this.props.containerColor ? this.props.containerColor : ''
         }`}
       >
-        <span className="mr-auto">{`${this.props.text}:`}</span>
-        <span className="">
+        <span className="mr-auto mb-2">{`${this.props.text}:`}</span>
+        <span className="flex flex-wrap">
           {this.props.data.map((type, index) => {
             return (
               <span
                 key={index}
                 style={{ backgroundColor: this.props.bg[index] }}
-                className={` border-2 border-stone-300 rounded-full mx-1 py-1 px-2 ${this.props.textColor} `}
+                className={`flex-wrap border-2 border-stone-300 rounded-full mx-1 py-1 px-2 ${this.props.textColor} `}
               >
                 {type}
               </span>
