432d997ea4f1fa268c0e6d942946b33f3260e6df
EricPohlers <53621659+EricPohlers@users.noreply.github.com>
2021-12-29 22:57:05 +0100

    grid fix

--- src/TemtemList.js
+++ src/TemtemList.js
@@ -4,7 +4,7 @@ import TemtemCard from './TemtemCard';
 export default class TemtemList extends Component {
   render() {
     return (
-      <div className="transition-all grid gap-4 lg:grid-cols-4  md:grid-cols-2 justify-evenly m-8">
+      <div className="transition-all grid gap-4 2xl: grid-cols-4 xl:grid-cols-3 lg:grid-cols-2 md:grid-cols-2 grid-cols-1 justify-evenly m-8">
         <React.StrictMode>
           {this.props.data.map((temtem) => {
             return (
